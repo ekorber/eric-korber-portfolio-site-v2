cd80ebb0b2d73390f9ad9304869d8337587bc813
ekorber <erickorber1994@gmail.com>
2023-03-22 16:33:12 -0400

    Create half page showcase piece component

---
+++ src/components/HalfPageShowcasePiece.js
@@ -0,0 +1,17 @@
+import EmbeddedVideoLink from "./EmbeddedVideoLink"
+
+function HalfPageShowcasePiece(props) {
+    return (
+        <div className="col-lg-6 my-5">
+            <h4 className="text-center">{props.title}</h4>
+
+            <EmbeddedVideoLink url={props.url} title={props.title} />
+
+            <p>{props.description}</p>
+
+            {props.useAdditionalText && <p className="text-center mt-3 mb-4">{props.additionalText}</p>}
+        </div>
+    );
+}
+
+export default HalfPageShowcasePiece;
