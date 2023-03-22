19fd93a644247e281891739bd6b0b762b5b6b76c
ekorber <erickorber1994@gmail.com>
2023-03-22 19:41:28 -0400

    Add and implement MainProjectsSection.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import HalfPageShowcasePiece from './components/HalfPageShowcasePiece';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import MainProjectsSection from './components/MainProjectsSection';
 
 function App() {
   return (
     <div className="App container">
-      
-      <HalfPageShowcasePiece
-        title="Rekill - Demo Footage"
-        url="https://www.youtube.com/embed/Q1dYVwZQlZI"
-        description="This is a first-person shooter demo made in Unreal Engine 5, with a style inspired by the legendary Doom series. It features multiple weapons, some of which deal extra damage to certain enemies, a levelling system with upgrades, varied enemy types with melee and ranged attacks, weak points on certain enemies, and multiple maps to play on. I used Blender to model and animate all the weapons and monsters, and Quixel Mixer to texture them."
-        useAdditionalText={true}
-        additionalText={<strong>Download for free on <a href="https://erickorber.itch.io/rekill" target="_blank" rel="noreferrer">Itch.io</a>.</strong>}
-      />
-      
+      <MainProjectsSection />
     </div>
   );
 }
---
+++ src/components/MainProjectsSection.js
@@ -0,0 +1,28 @@
+import HalfPageShowcasePiece from "./HalfPageShowcasePiece";
+
+function MainProjectsSection() {
+    return (
+        <div className="row">
+
+            <h2 class="text-center mt-5">MAIN PROJECTS</h2>
+
+            <HalfPageShowcasePiece
+                title="Rekill - Demo Footage"
+                url="https://www.youtube.com/embed/Q1dYVwZQlZI"
+                description="This is a first-person shooter demo made in Unreal Engine 5, with a style inspired by the legendary Doom series. It features multiple weapons, some of which deal extra damage to certain enemies, a levelling system with upgrades, varied enemy types with melee and ranged attacks, weak points on certain enemies, and multiple maps to play on. I used Blender to model and animate all the weapons and monsters, and Quixel Mixer to texture them."
+                useAdditionalText={true}
+                additionalText={<strong>Download for free on <a href="https://erickorber.itch.io/rekill" target="_blank" rel="noreferrer">Itch.io</a>.</strong>}
+            />
+
+            <HalfPageShowcasePiece
+                title="Third Person Action, made in Unity"
+                url="https://www.youtube.com/embed/XBOdHimLgac"
+                description="This is a short boss battle demo made in Unity. I used Blender to model and animate the boss, as well as the various dessert-related props. It was themed to be a creepier version of the famous poem Inferno, by Dante Alighieri."
+                useAdditionalText={false}
+            />
+            
+        </div>
+    );
+}
+
+export default MainProjectsSection;
