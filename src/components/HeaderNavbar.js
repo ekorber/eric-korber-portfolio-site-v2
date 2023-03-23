a175a69df3f6ec81e1bea33a3bcb570fd442c22d
ekorber <erickorber1994@gmail.com>
2023-03-22 22:49:10 -0400

    Update header with currently-empty links

--- src/components/HeaderNavbar.js
+++ src/components/HeaderNavbar.js
@@ -6,8 +6,15 @@ import Nav from 'react-bootstrap/Nav';
 function HeaderNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark">
-            <Container className='justify-content-center'>
+            <Container>
                 <Navbar.Brand href="#home" className="navbar-text">Eric's Portfolio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                    <Nav>
+                        <Nav.Link href="#home" className="navbar-link">Game Development</Nav.Link>
+                        <Nav.Link href="#home" className="navbar-link">About Me</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
             </Container>
         </Navbar>
     );
