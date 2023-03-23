65f65701947bb2564120fa8c504c28c8c0840092
ekorber <erickorber1994@gmail.com>
2023-03-22 23:27:32 -0400

    Add routing functionality to header

--- src/components/HeaderNavbar.js
+++ src/components/HeaderNavbar.js
@@ -2,17 +2,18 @@ import './HeaderNavbar.css';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 function HeaderNavbar() {
     return (
         <Navbar collapseOnSelect expand="lg" variant="dark">
             <Container>
-                <Navbar.Brand href="#home" className="navbar-text">Eric's Portfolio</Navbar.Brand>
+                <Navbar.Brand className="navbar-brand">Eric's Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
                     <Nav>
-                        <Nav.Link href="#home" className="navbar-link">Game Development</Nav.Link>
-                        <Nav.Link href="#home" className="navbar-link">About Me</Nav.Link>
+                        <Link to="/" className="navbar-link">Game Development</Link>
+                        <Link to="/about" className="navbar-link">About Me</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
