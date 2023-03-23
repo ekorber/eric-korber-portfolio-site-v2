2f2c00cf380e75b6d544e9b01649c105f00aeff6
ekorber <erickorber1994@gmail.com>
2023-03-22 20:25:41 -0400

    Add side projects section to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MainProjectsSection from './components/MainProjectsSection';
+import SideProjectsSection from './components/SideProjectsSection';
 
 function App() {
   return (
     <div className="App container">
       <MainProjectsSection />
+      <hr />
+      <SideProjectsSection />
     </div>
   );
 }
