5139f13d00fe582020be8344853c62bb309fdf18
ekorber <erickorber1994@gmail.com>
2023-03-22 20:30:42 -0400

    Fix weird-looking spacing at bottom of component

--- src/components/HalfPageShowcasePiece.js
+++ src/components/HalfPageShowcasePiece.js
@@ -2,7 +2,7 @@ import EmbeddedVideoLink from "./EmbeddedVideoLink"
 
 function HalfPageShowcasePiece(props) {
     return (
-        <div className="col-lg-6 my-5">
+        <div className="col-lg-6 mt-5 mb-4">
             <h4 className="text-center">{props.title}</h4>
 
             <EmbeddedVideoLink url={props.url} title={props.title} />
