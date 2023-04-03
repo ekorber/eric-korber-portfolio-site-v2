58112f837e87979ba65fdeb91c778c027882e56a
ekorber <erickorber1994@gmail.com>
2023-04-02 23:29:22 -0400

    Added Bootstrap to skills list

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -34,6 +34,7 @@ function AboutPage() {
                     <ul className="list-group">
                         <li className="list-group-item">HTML</li>
                         <li className="list-group-item">CSS</li>
+                        <li className="list-group-item">Bootstrap</li>
                         <li className="list-group-item">Javascript</li>
                         <li className="list-group-item">React.js</li>
                         <li className="list-group-item">Amazon Web Services</li>
