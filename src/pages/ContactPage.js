2161f51ac3093a181b11869ca3eacc3080a9227a
ekorber <erickorber1994@gmail.com>
2023-04-11 13:15:56 -0400

    Fill out contact page with content

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,6 +1,13 @@
+import Form from "../components/Form";
+
 function ContactPage() {
     return (
-        <h2 className="container text-center my-5">Contact Me</h2>
+        <div className="container">
+            <h2 className="text-center my-5">Reach Out To Me!</h2>
+            <p className="text-center">Feel free to contact me if you have any questions, or would like to discuss any work-related opportunties! I'm quick to respond, and would love to get in touch.</p>
+
+            <Form />
+        </div>
     );
 }
 
