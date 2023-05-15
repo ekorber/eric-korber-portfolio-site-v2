559b3c9af7d3114267001e70b24de6d7acee3054
ekorber <erickorber1994@gmail.com>
2023-05-15 13:00:12 -0400

    Create empty web portfolio page

---
+++ src/pages/WebDevPortfolioPage.js
@@ -0,0 +1,11 @@
+function WebDevPortfolioPage() {
+    return (
+        <div className='container'>
+            <div className="row">
+                <h2 className="text-center mt-5">MAIN PROJECTS</h2>
+            </div>
+        </div>
+    );
+}
+
+export default WebDevPortfolioPage;
