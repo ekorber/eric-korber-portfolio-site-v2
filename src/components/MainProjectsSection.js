02a19c866e1b5079e39c67eb6febc00307f67442
Eric Korber <erickorber1994@gmail.com>
2024-07-03 17:37:29 -0400

    Tweak text

--- src/components/MainProjectsSection.js
+++ src/components/MainProjectsSection.js
@@ -21,7 +21,7 @@ function MainProjectsSection() {
             </div>
 
             <HalfPageShowcasePiece
-                title="A Doom-inspired FPS in Unreal Engine 5"
+                title="Doom-inspired FPS in Unreal Engine 5"
                 url="https://www.youtube.com/embed/Q1dYVwZQlZI"
                 videoMetaTitle="Rekill - Demo Footage"
                 description="This is a first-person shooter demo made in Unreal Engine 5, with a style inspired by the legendary Doom series. It features multiple weapons, some of which deal extra damage to certain enemies, a levelling system with upgrades, varied enemy types with melee and ranged attacks, weak points on certain enemies, and multiple maps to play on. I used Blender to model and animate all the weapons and monsters, and Quixel Mixer to texture them."
